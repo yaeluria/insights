b4b8543e7d7e51448b388cfe1214c747cb286865
Yael Luria <yaeluria@gmail.com>
2020-10-11 11:49:51 +0300

    implement routing, responsive narrower styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
-import Header from './components/Header';
-import InsightsContainer from './components/InsightsContainer';
+import Header from "./components/Header";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import InsightsContainer from "./components/InsightsContainer";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <InsightsContainer />
+    <div className="root">
+      <div className="narrow">
+        <Router>
+          <Header />
+          <NavBar />
+          <Switch>
+            <Route exact path="/">
+              <h1>רקע</h1>
+            </Route>
+            <Route path="/answers">
+              <h1>תשובות</h1>
+            </Route>
+            <Route path="/insights">
+              <InsightsContainer />
+            </Route>
+            <Route path="/decisions">
+              <h1>החלטות</h1>
+            </Route>
+            <Route path="/insight/:id">
+              <h1>single insight</h1>
+            </Route>
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
